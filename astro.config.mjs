379e0cb4d5ab43d7850ec29b2d7486408534a1f5
maru-star <maruoka@web-trickster.com>
2023-11-15 16:45:31 +0900

    sassを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,35 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
-  
+  base: "",
+  outDir: './dist',
+  server: {
+    open: true,
+  },
+  // integrations: [
+  //   compress({}),
+  //   relativeLinks(),
+  // ],
+  // vite: {
+  //   build: {
+  //     rollupOptions: {
+  //       output: {
+  //         assetFileNames: (assetInfo) => {
+  //           let extType = assetInfo.name.split('.')[1];
+  //           if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+  //             extType = 'fonts';
+  //           }
+  //           if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+  //             extType = 'images';
+  //           }
+  //           if (extType === 'scss') {
+  //             return `assets/scss/style.css`;
+  //           }
+  //           return `assets/${extType}/[name][extname]`;
+  //         },
+  //       },
+  //       entryFileNames: 'assets/js/[name].js',
+  //     },
+  //   },
+  // },
 });
